687ec548715da616d2cb47e0778b87b3768fa7f1
kundan Bapat <kundan.bapat@gmail.com>
2022-01-02 07:14:43 -0500

    once again remove client code

--- server/dist/dictResolver.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.resolvers = void 0;
-var dictSchema_1 = require("./schema/dictSchema");
-exports.resolvers = {
-    Query: {
-        word: function () { return dictSchema_1.wordInfoSample; },
-    },
-};
